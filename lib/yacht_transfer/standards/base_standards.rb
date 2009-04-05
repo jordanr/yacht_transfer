154a4b6f00fe497f30eee90cf1856f8c1e1c2d51
Richard <richard@jordanyachts.com>
2009-04-05 12:34:18 -0400

    adding few extra specs

--- lib/yacht_transfer/standards/base_standards.rb
+++ lib/yacht_transfer/standards/base_standards.rb
@@ -5,6 +5,10 @@ module YachtTransfer
 #        model.extend(ClassMethods)
       end
 
+      ##########################
+      # Main Transform
+      ########################
+
       KEY_TRANSFORM = {		:username=> {:yw=>:url, :yc=>""},
 				:id => {:yw=> :boat_id, :yc=>:vessels_id},
 				:price => {:yw=>:price, :yc=> :ask_price},
@@ -13,9 +17,32 @@ module YachtTransfer
 				:yacht_specification_length => {:yw=> :length, :yc=> :length},
 				:yacht_specification_manufacturer => {:yw=> :maker, :yc=> :vessel_manufacturer_name},
 				:yacht_specification_model=> {:yw=>:model, :yc=>:model},
-				:yacht_specification_year=> {:yw=>:year, :yc=>:built_in_year}
+				:yacht_specification_year=> {:yw=>:year, :yc=>:built_in_year},
+				:yacht_specification_material=> {:yw=>:hull_material, :yc=>:hull_materials_id},
+				:yacht_specification_fuel=> {:yw=>:fuel, :yc=> :fuel_types_id},
+				:yacht_specification_number_of_engines => {:yw=>:engine_num, :yc=>:number_of_engines },
 			}
       def key_transform(key, site); KEY_TRANSFORM[key.to_sym][site.to_sym]; end
+
+
+      ######################
+      # Value Transforms
+      ######################
+
+      MATERIAL_TRANSFORM = {
+			:composite => {}
+			:fiberglass => {}
+			:steel => {}
+			:wood => {}
+			:other => {}
+			}
+      FUEL_TRANSFORM = {
+			:gas => {}
+			:diesel => {}
+			:other => {}
+			}
+      NUMBER_OF_ENGINES_TRANSFORM = {}
+
     end
   end
 end
